bf77a4c2f5fec57cb8210fcdfe75c5032c5cc830
athenascripts <108603677+athenascripts@users.noreply.github.com>
2022-10-19 15:34:33 -0700

    Fixed some errors
    I wrote this code at like 3 am o_o

--- client_message_color_lib/lua/autorun/sh_clientmessage_core.lua
+++ client_message_color_lib/lua/autorun/sh_clientmessage_core.lua
@@ -1,13 +1,13 @@
 -- EXAMPLE BELOW (SERVERSIDE ONLY)
--- ply:SendColoredChat({Color(255,0,0), "This is red! ", Color(0,0,255), "And this is blue!"})
+-- ply:SendColoredChat(Color(255,0,0), "This is red! ", Color(0,0,255), "And this is blue!")
 
 if SERVER then
     util.AddNetworkString("AthenaTools.SendColoredMessage")
 
     local meta = FindMetaTable("Player")
 
-    function meta:SendColoredChat(tbl)
-        if not tbl or not istable(tbl) then return end
+    function meta:SendColoredChat(...)
+        local tbl = {...}
 
         net.Start("AthenaTools.SendColoredMessage")
             net.WriteTable(tbl)
